cf80c9f7bcfd8354f331f7d7c5d255a5396d4f0d
vailphoria <vailphoria@gmail.com>
2020-05-04 22:49:45 +0300

    Initial commit

---
+++ HelloWorld/src/Playground.java
@@ -0,0 +1,12 @@
+/**
+ * @author Valeri
+ *
+ */
+public class Playground {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!!!!");
+	}
+
+}
