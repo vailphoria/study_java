a5fe77eb16c3f1849528b54b80a5da48082b4053
vailphoria <vailphoria@gmail.com>
2020-05-05 17:09:32 +0300

    test split

--- HelloWorld/src/Playground.java
+++ HelloWorld/src/Playground.java
@@ -4,10 +4,17 @@
  */
 public class Playground {
 
+	public static int findShort(String s) {
+		int n = s.length();
+		for (String retval : s.split(" ")) {
+        	if (retval.length()< n) {
+        		n = retval.length();
+        	}
+        }
+		return n;		
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world!!!!");
-		System.out.println("Hello world 2 !!!!");
+
 	}
 
 }
