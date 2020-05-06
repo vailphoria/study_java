87f201ab51b18c00b8b79a61ed901cac226fc084
vailphoria <vailphoria@gmail.com>
2020-05-06 21:33:26 +0300

    Duplicate Encoder

--- HelloWorld/src/Playground.java
+++ HelloWorld/src/Playground.java
@@ -3,18 +3,21 @@
  *
  */
 public class Playground {
-    public static String whoLikesIt(String[] names) {
-    	
-        switch (names.length) {
-        case 0: return "no one likes this";
-        case 1: return names[0]+" likes this";
-        case 2: return names[0]+" and "+ names[1]+" like this";
-        case 3: return names[0]+", "+ names[1]+" and "+ names[2]+" like this";
-        default: return names[0]+", "+ names[1]+" and "+ (names.length-2) +" others like this";
-        }
-    }
+	static String encode(String word){
+		word = word.toLowerCase();
+		char[] arr = new char[word.length()];
+		for (int i=0;i<word.length();i++) {
+			int count=0;
+			for (int j=0;j<word.length();j++) {
+				if(word.charAt(i)==word.charAt(j))count++;
+			}
+			if (count>=2)arr[i]=')';
+			else arr[i]='(';
+		}
+		return word.copyValueOf(arr);
+	}
 	public static void main(String[] args) {
-		System.out.printf("My %s %d world",47,"lovely");
+		System.out.print(Playground.encode("Prespecialized"));
 	}
 
 }
