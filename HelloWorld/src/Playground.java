f745c1e5b53512ceeaaec32db38ab2bbfd4c155f
vailphoria <vailphoria@gmail.com>
2020-05-06 20:43:01 +0300

    likes

--- HelloWorld/src/Playground.java
+++ HelloWorld/src/Playground.java
@@ -3,16 +3,18 @@
  *
  */
 public class Playground {
-	public int solution(int number) {
-		int sum = 0;
-		for (int i = 0; i<number; i+=3) sum+=i;
-		for (int i = 0; i<number; i+=5) {
-			if (i%15 == 0)continue;
-			else sum+=i;
-		}
-	    return sum;
-		}
+    public static String whoLikesIt(String[] names) {
+    	
+        switch (names.length) {
+        case 0: return "no one likes this";
+        case 1: return names[0]+" likes this";
+        case 2: return names[0]+" and "+ names[1]+" like this";
+        case 3: return names[0]+", "+ names[1]+" and "+ names[2]+" like this";
+        default: return names[0]+", "+ names[1]+" and "+ (names.length-2) +" others like this";
+        }
+    }
 	public static void main(String[] args) {
+		System.out.printf("My %s %d world",47,"lovely");
 	}
 
 }
