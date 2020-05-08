28f497f49a057b55ca8df7d4f24b02053c374db2
vailphoria <vailphoria@gmail.com>
2020-05-08 12:46:02 +0300

    reading txt and count words

--- HelloWorld/src/Playground.java
+++ HelloWorld/src/Playground.java
@@ -2,17 +2,19 @@ import java.io.*;
 
 public class Playground {
 	public static void main(String[] args) {
-        
-		try(FileWriter writer = new FileWriter("notes3.txt", true))
+        String text ="";
+        int words = 0;
+		try(FileReader reader = new FileReader("notes3.txt"))
         {
-           // запись всей строки
-            String text = "Hello Gold!";
-            writer.write(text);
-            // запись по символам
-            writer.append('\n');
-            writer.append('E');
-             
-            writer.flush();
+            int c;
+            while((c=reader.read())!=-1){
+                 
+                text += (char)c;
+            }
+            String[] subStr;
+            subStr = text.split(" ");
+            System.out.println(subStr.length);
+
         }
         catch(IOException ex){
              
